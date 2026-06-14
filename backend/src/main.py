683f057a6b61dd8602d40e6ece2fc8cd2b8dd478
Kedarini <kedardamps@gmail.com>
2026-06-14 14:54:50 +0200

    added functions to manipulate todo list through the API

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 origins = [
     "http://localhost:4200",
@@ -15,6 +16,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+todo_list = ["Kupić mleko", "Skonfigurować Postgresa", "Napisać testy"]
+
+class Task(BaseModel):
+    task: str
+
 @app.get("/api/data")
 def get_data():
-    return {"message": "Siemanko z FastAPI!", "status": "success"}
+    return todo_list
+
+@app.post("/api/tasks")
+def create_task(payload: Task):
+    todo_list.append(payload.task)
+    return todo_list
+
+@app.delete("/api/tasks/{task_id}")
+def delete_task(task_id: int):
+    if 0 <= task_id < len(todo_list):
+        todo_list.pop(task_id)
+        return todo_list
+    raise HTTPException(status_code=404, detail="Task not found")
